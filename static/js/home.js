d75acc9a1d1ec4bae9d5743b93792415cc3ca2a3
hayoung <hayounglala@jbnu.ac.kr>
2024-08-10 19:06:55 +0900

    real-data-test

--- static/js/home.js
+++ static/js/home.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const recommendationBtn = document.getElementById('recommendation-btn');
 
   recommendationBtn.addEventListener('click', () => {
-      const customer_id = "0";
+      const customer_id = "624";
       window.location.href = `recommendation.html?customer_id=${customer_id}`;
   });
 });
--- static/js/interpreter.js
+++ static/js/interpreter.js
@@ -1,78 +1,84 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const titleArea = document.getElementById('title');
-    const periodDiv = document.getElementById('period');
-    const imgDiv = document.getElementById('img');
-    const descriptionDiv = document.getElementById('description');
-    const backButton = document.getElementById('back-btn');
+  const titleArea = document.getElementById('title');
+  const periodDiv = document.getElementById('period');
+  const imgDiv = document.getElementById('img');
+  const descriptionDiv = document.getElementById('description');
+  const backButton = document.getElementById('back-btn');
 
-    const params = new URLSearchParams(window.location.search);
-    const eventId = params.get('event_id');
+  const params = new URLSearchParams(window.location.search);
+  const eventId = params.get('event_id');
 
-    fetch('http://127.0.0.1:5000/api/data/event', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: eventId
+  fetch('http://127.0.0.1:5000/api/data/event', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: eventId,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Success:', data);
+      console.log(data);
+      const eventTitle = data['event']['event_title'];
+      const eventPeriod = data['event']['event_period'];
+      const eventDescription = data['event']['text'];
+      const dictionary = data['dictionary'];
+      const eventImg = data['event']['event_img'];
+
+      titleArea.innerHTML = eventTitle;
+      periodDiv.innerHTML = eventPeriod;
+      imgDiv.innerHTML = insertImgs(eventImg);
+      descriptionDiv.innerHTML = highlightWords(
+        eventTitle,
+        eventDescription,
+        dictionary
+      );
+      addTooltipClickEvent();
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
+    .catch((error) => console.error('Error sending data:', error));
 
-        const eventTitle = data["event"]["event_title"];
-        const eventPeriod = data["event"]["event_period"];
-        const eventDescription = data["event"]["text"];
-        const dictionary = data["dictionary"];
-        const eventImg = data["event"]["event_img"];
+  function insertImgs(eventImg) {
+    let insertedImgs = '';
 
-        titleArea.innerHTML = eventTitle;
-        periodDiv.innerHTML = eventPeriod;
-        imgDiv.innerHTML = insertImgs(eventImg);
-        descriptionDiv.innerHTML = highlightWords(eventTitle, eventDescription, dictionary);
-        addTooltipClickEvent();
-    })
-    .catch(error => console.error('Error sending data:', error));
+    eventImg.forEach((url) => {
+      insertedImgs += `<img src="${url}" width="90%">`;
+    });
+    return insertedImgs;
+  }
 
-    function insertImgs(eventImg) {
-        let insertedImgs = '';
+  function highlightWords(eventTitle, text, words) {
+    let highlightedText = text;
 
-        eventImg.forEach(url => {
-            insertedImgs += `<img src="${url}" width="90%">`;
-        });
-        return insertedImgs;
-    }
+    Object.entries(words).forEach(([key, value]) => {
+      console.log(`${key}: ${value}`);
+      const regex = new RegExp(`(${key})`, 'g');
 
-    function highlightWords(eventTitle, text, words) {
-        let highlightedText = text;
+      if (highlightedText.search(regex)) {
+        highlightedText = highlightedText.replace(
+          regex,
+          `<span class="tooltip" data-meaning="${value}"><b>${key}</b></span>`
+        );
+      }
+    });
 
-        Object.entries(words).forEach(([key, value]) => {
-            console.log(`${key}: ${value}`);
-            const regex = new RegExp(`(${key})`, 'g');
+    highlightedText = highlightedText.replace(/\n/g, '<br>');
+    return highlightedText;
+  }
 
-            if (highlightedText.search(regex)) {
-                highlightedText = highlightedText.replace(
-                regex,
-                `<span class="tooltip" data-meaning="${value}"><b>${key}</b></span>`);
-            }
-        });
+  function addTooltipClickEvent() {
+    const tooltips = document.querySelectorAll('.tooltip');
+    tooltips.forEach((tooltip) => {
+      tooltip.addEventListener('click', () => {
+        const searchQuery = tooltip.textContent.trim();
+        const searchUrl = `https://search.naver.com/search.naver?query=${encodeURIComponent(
+          searchQuery
+        )}`;
+        window.open(searchUrl, '_blank');
+      });
+    });
+  }
 
-        highlightedText = highlightedText.replace(/\n/g, '<br>');
-        return highlightedText;
-    }
-
-    function addTooltipClickEvent() {
-        const tooltips = document.querySelectorAll('.tooltip');
-        tooltips.forEach(tooltip => {
-            tooltip.addEventListener('click', () => {
-                const searchQuery = tooltip.textContent.trim();
-                const searchUrl = `https://search.naver.com/search.naver?query=${encodeURIComponent(searchQuery)}`
-                window.open(searchUrl, '_blank');
-            });
-        });
-    }
-
-    backButton.addEventListener('click', function () {
-        window.history.back();
-    })
-
+  backButton.addEventListener('click', function () {
+    window.history.back();
+  });
 });
